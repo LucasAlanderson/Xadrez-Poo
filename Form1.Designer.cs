853be1b43459996077efe2267062c8f282e0c07c
Diogo Conceição dos Santos <20231170150047@ifrn.local>
2025-02-04 17:37:03 -0300

    Amo POO

---
+++ Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace Xadrez;
+
+partial class Form1
+{
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+  
+    private const int TamanhoDaMatriz = 8;
+    private Button[,] matriz = new Button[TamanhoDaMatriz, TamanhoDaMatriz];
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        this.components = new System.ComponentModel.Container();
+        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        this.ClientSize = new System.Drawing.Size(800, 450);
+        this.Text = "Xadrez";
+        for(int linha = 0; linha < TamanhoDaMatriz; linha++){
+            for(int coluna = 0; coluna <TamanhoDaMatriz; coluna++){
+                Button b = new Button();
+                if((coluna + linha)%2 == 0) {
+                    b.Size = new Size(50, 50);
+                    b.Location = new Point(50*linha, 50*coluna);
+                    b.BackColor = Color.White;
+                    matriz[linha,coluna] = b;
+                    this.Controls.Add(b);
+                }
+                // }else if(linha + coluna == ) {
+                //     b.Size = new Size(50, 50);
+                //     b.Location = new Point(50*linha, 50*coluna);
+                //     b.BackColor = Color.Gray;
+                //     matriz[linha,coluna] = b;
+                // }
+                else{
+                    b.Size = new Size(50, 50);
+                    b.Location = new Point(50*linha, 50*coluna);
+                    b.BackColor = Color.Gray;
+                    matriz[linha,coluna] = b;
+                }
+                this.Controls.Add(b);
+            }
+    }
+
+    #endregion
+}
+}
---
+++ Pecas.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Xadrez
+{
+    public class Pecas : Form
+    {
+
+    }
+}
